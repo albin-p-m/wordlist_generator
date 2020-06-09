64114bad01148e86550d93f7604a8d048ada090a
mr.infinite <albinalbuss@gmail.com>
2020-06-09 17:42:36 +0000

    changed all string declaration as char

--- wordlist generator in c.c
+++ wordlist generator in c.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 #include<string.h>
 void main(){
-    string alphabets = ascii.lowercase + ascii.uppercase + ascii.punctions + 'EOF';
+    char alphabets[] = ascii.lowercase + ascii.uppercase + ascii.punctions;
     int limit = 8;
 
     while(limit > 7 && limit < 13){
-        for(string i = 'a'; i != 'EOF'; i++){
+        for(char i = 'a'; i != '\0'; i++){
             string word ='';
             word += i;
-            for(string j = 'a'; word.length != limit; j++){
+            for(char j = 'a'; word.length != limit; j++){
                 word += j;
             }
             printf("%s",word);
