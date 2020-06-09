d1072eef91188d9d3fff2599353bebc8ea56fc16
mr.infinite <albinalbuss@gmail.com>
2020-06-09 18:55:48 +0000

    added few changes in the main loop

--- wordlist generator in c.c
+++ wordlist generator in c.c
@@ -1,20 +1,24 @@
 #include<stdio.h>
 #include<string.h>
-void main(){
-    char word[100];
 
-    int j = 0;                                      //used to iterate though word's indexes
+
+void main(){
+    char characters[100];                           //the character set generated
+    char word[13];                                  //the word generated
+    int limit = 8;                                  //initial limit of the word generated
+    int j = 0;                                      //used to iterate through character's indexes
+
+
     for(int i = 32; i < 127; i++){                  //for getting whole characters into the word array
         char ch = i;
-        word[j] = ch;
+        characters[j] = ch;
         j++;
     }
-    int limit = 8;
 
     while(limit > 7 && limit < 13){
-        for(char i = 'a'; i != '\0'; i++){
-            word ='';
-            word += i;
+        int i = 0;                                                 //variable to iterate through characater array
+        for(characters[i] = ' '; characters[i] != '\0'; i++){
+            strncat(word, character[i]);
             for(char j = 'a'; word.length != limit; j++){
                 word += j;
             }
