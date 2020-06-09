1d7e83529c5bd96bd77b8d5bc64d7565cc2eda59
mr.infinite <albinalbuss@gmail.com>
2020-06-09 18:32:01 +0000

    add a for loop to get whole characters

--- wordlist generator in c.c
+++ wordlist generator in c.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
 #include<string.h>
 void main(){
-    char alphabets[] = ascii.lowercase + ascii.uppercase + ascii.punctions;
+    char word[100];
+
+    int j = 0;                                      //used to iterate though word's indexes
+    for(int i = 32; i < 127; i++){                  //for getting whole characters into the word array
+        char ch = i;
+        word[j] = ch;
+        j++;
+    }
     int limit = 8;
 
     while(limit > 7 && limit < 13){
         for(char i = 'a'; i != '\0'; i++){
-            string word ='';
+            word ='';
             word += i;
             for(char j = 'a'; word.length != limit; j++){
                 word += j;
