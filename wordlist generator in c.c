3e7fadf7be4801182452745d7c5ee3b98a14b572
mr.infinite <albinalbuss@gmail.com>
2020-06-14 16:41:20 +0000

    new logic started

--- wordlist generator in c.c
+++ wordlist generator in c.c
@@ -1,33 +1,31 @@
 #include<stdio.h>
 #include<string.h>
 
-
 void main(){
     char characters[100];                           //the character set generated
     char word[13];                                  //the word generated
     int limit = 8;                                  //initial limit of the word generated
-    int j = 0;                                      //used to iterate through character's indexes
-
+    int j = 0, count, last;                         //used to iterate through character's indexes
 
     for(int i = 32; i < 127; i++){                  //for getting whole characters into the word array
         char ch = i;
-        characters[j] = ch;
-        j++;
+        characters[count] = ch;
+        count++;
+        last = count - 1;
     }
     printf("%s",characters);
 
-    while(limit > 7 && limit < 13){
-        int i = 0;                                                 //variable to iterate through characater array
-        while(characters[i] != '\0'){
-            char temp = characters[i];
-            strcat(word, &temp);
-            if(strlen(word) > 12){
-                printf("\n%s",word);
-                break;
-            }
-            i++;
-            
+    for(int i = 0; i < limit; i++){                 //initialize the word array with the first character in character array.
+        word[i] = characters[0];
+    }
+
+    for((limit > 7) && (limit < 13);){
+        label;
+        last = limit;
+        for(int i = 0; i <= last; i++){
+            word[limit] = characters[k];
+            printf("%s", word);
         }
-        limit++;
+
     }
 }
