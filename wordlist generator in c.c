260ab412d42e5ec39c089467867418e3de374109
mr.infinite <albinalbuss@gmail.com>
2020-06-15 10:24:21 +0000

    some improvements added to the old code

--- wordlist generator in c.c
+++ wordlist generator in c.c
@@ -4,28 +4,38 @@
 void main(){
     char characters[100];                           //the character set generated
     char word[13];                                  //the word generated
-    int limit = 8;                                  //initial limit of the word generated
-    int j = 0, count, last;                         //used to iterate through character's indexes
+    int limit = 8, count = 0;                       //initial limit of the word generated and a counting variable for knowing the last index of character array
 
     for(int i = 32; i < 127; i++){                  //for getting whole characters into the word array
         char ch = i;
         characters[count] = ch;
         count++;
-        last = count - 1;
     }
-    printf("%s",characters);
+    printf("\n\n%s\n\n",characters);
 
     for(int i = 0; i < limit; i++){                 //initialize the word array with the first character in character array.
         word[i] = characters[0];
     }
 
-    for((limit > 7) && (limit < 13);){
-        label;
-        last = limit;
-        for(int i = 0; i <= last; i++){
-            word[limit] = characters[k];
-            printf("%s", word);
+    while((limit > 7)){ //&& (limit < 13)){
+        int last = limit;
+        for(int i = 0; i < count; i++){
+            word[limit] = characters[i];
+            printf("%s\n", word);
+            for(int j = limit; j >= 0; j--){
+                if(word[j] != characters[count - 1]){
+                    for(int k = 0; k < count; k++){
+                        if(word[j] == characters[k]){
+                            word[j] = characters[k + 1];
+                        }
+                    }
+                    break;
+                }
+            }
         }
-
+        if(word[0] == characters[count]){
+        limit++;
+        }
+        break;
     }
 }
